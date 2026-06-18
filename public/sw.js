dd73723ad34678149c3587fe9cf23b57154742ac
Amely <amely.attias@gmail.com>
2026-06-18 16:35:07 +0000

    feat: bouton J'ai déjà un compte + bouton installer PWA (Android auto, iOS instructions)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "drac-1781800418"
+const CACHE = "drac-1781800507"
 self.addEventListener("install", e => { self.skipWaiting() })
 self.addEventListener("activate", e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()))
