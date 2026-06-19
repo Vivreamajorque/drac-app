2fc927a1685dac243da7df5db80625f968bafb90
Amely <amely.attias@gmail.com>
2026-06-19 05:37:04 +0000

    fix: nouveau timestamp SW pour vider le cache corrompu

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "drac-1781822851"
+const CACHE = "drac-1781847421"
 self.addEventListener("install", e => { self.skipWaiting() })
 self.addEventListener("activate", e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()))
