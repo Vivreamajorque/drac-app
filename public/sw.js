337d503c57cb0cd55f7d039e2a12b5ea6f081ee9
Amely <amely.attias@gmail.com>
2026-06-19 06:20:36 +0000

    fix: supprimer 12 addEventListener sur IDs orphelins qui bloquaient tout le JS

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "drac-1781848888"
+const CACHE = "drac-1781850036"
 self.addEventListener("install", e => { self.skipWaiting() })
 self.addEventListener("activate", e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()))
